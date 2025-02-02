1e6742edf603c4ef0a9da766bd86ab76f0a18726
ikrzywda <krzywdaigor890@gmail.com>
2025-02-02 10:41:20 +0100

    removed old frontend

--- frontend/src/app/task-log/store/index.ts
+++
@@ -1,3 +0,0 @@
-export { taskLogReducer, TASK_LOG_FEATURE_KEY } from './task-log.reduder';
-export type { TaskLogState } from './task-log.reduder';
-export * as taskLogEffects from './task-log.effects';
--- frontend/src/app/task.ts
+++
@@ -1,9 +0,0 @@
-export interface TaskDraft {
-  title: string;
-  contents: string;
-  durationSeconds: number;
-}
-
-export interface Task extends TaskDraft {
-  createdAt: Date;
-}
