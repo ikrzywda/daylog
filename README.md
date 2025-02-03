b7d3eae58a11df14f8dbdcdc1f55b347ca019d97
ikrzywda <krzywdaigor890@gmail.com>
2025-02-03 01:07:05 +0100

    added readme

---
+++ README.md
@@ -0,0 +1,27 @@
+# DayLog
+
+## Application for keeping track of tasks throughout the day
+
+## Checklist
+
+- [x] Project boilerplate
+- [x] CRUD on tasks
+- [ ] Displaying tasks within selected time range
+- [ ] Charts with rundowns
+- [ ] Original estimation field in tasks
+- [ ] Markdown support for the contents
+- [ ] SQLite
+- [ ] Import / export of data
+- [ ] Dockerization
+
+## Technology
+
+### Frontend
+
+- AngularJS
+- ngrx
+- Material
+
+### Backend
+
+- FastAPI
--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { TaskLogModule } from './task-log/task-log.module';
+import { MatToolbarModule } from '@angular/material/toolbar';
 import { TaskListComponent } from './task-log/components/task-list/task-list.component';
-import { TaskInputDialog } from './task-log/components/task-input-dialog/task-input-dialog.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, TaskListComponent, TaskInputDialog],
+  imports: [RouterOutlet, TaskLogModule, MatToolbarModule, TaskListComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
